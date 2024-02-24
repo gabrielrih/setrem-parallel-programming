f2b4ac05bde05552446c9493c1cdac8e76803704
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2024-02-24 10:32:10 -0300

    feat: increment the compile script

--- exercise/compile_and_run.sh
+++ exercise/compile_and_run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 num_processes=$1
 if ! [ "$num_processes" ]; then num_processes=4; fi  # default value
+run_on_cluster=$2
+if ! [ "$run_on_cluster" ]; then run_on_cluster=1; fi  # default value
 
 exe_file='tmp_exe'
 file='farm/exercise.cpp'
@@ -9,7 +11,12 @@ echo "(+) Compiling"
 if [ -f "$exe_file" ]; then rm $exe_file; fi
 mpic++ -Wall -std=c++1y -O3 $file -o $exe_file
 
-echo "(+) Running..."
-mpirun -np $num_processes --machinefile /home/mpihpc/.cluster_hostfile $exe_file
+if [ $run_on_cluster == 1 ]; then
+    echo "(+) Running on cluster..."
+    mpirun -np $num_processes --machinefile /home/mpihpc/.cluster_hostfile $exe_file
+else
+    echo "(+) Running on the current machine..."
+    mpirun -np $num_processes $exe_file
+fi
 
 exit 0

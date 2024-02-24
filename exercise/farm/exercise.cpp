4bbd13c455d656d9d726812231aa462a94a442b2
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2024-02-24 10:13:25 -0300

    fix: validation for min of processes

--- exercise/farm/exercise.cpp
+++ exercise/farm/exercise.cpp
@@ -51,10 +51,9 @@ int main(int argc, char *argv []){
     // Validation
     int min_of_processes=WORKERS_QUANTITY + 2;  // emitter, collector and workers
     if (numprocs < min_of_processes) {
-        throw std::invalid_argument("The quantity of informed processes are to low!");
         MPI_Finalize();
+        cout << "ERROR: The quantity of informed processes are to low!" << std::endl;
         return 1;
-        //exit(1);
     }
 
     // Create the workers rank

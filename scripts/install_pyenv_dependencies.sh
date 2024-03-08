8c1b58c7873aacb7b9d1a3bc7f1414da5c5f9148
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2024-03-08 16:46:41 -0300

    feat: pyenv dependencies

---
+++ scripts/install_pyenv_dependencies.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt update
+sudo apt install \
+    build-essential \
+    curl \
+    libbz2-dev \
+    libffi-dev \
+    liblzma-dev \
+    libncursesw5-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    llvm \
+    make \
+    tk-dev \
+    wget \
+    xz-utils \
+    zlib1g-dev

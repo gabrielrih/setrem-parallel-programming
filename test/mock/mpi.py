85b20fb21d8110729b329f7fd69ff37360037e89
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2024-03-09 15:56:29 -0300

    fix: add some comments to help

--- test/mock/mpi.py
+++ test/mock/mpi.py
@@ -13,8 +13,14 @@ class DummyMPI:
 
     def send(self, *args, **kwargs):
         # fix it
+        # for emitter, it don't need to do anything
+        # for collector, it doesn't matter
+        # for worker, it don't need to do anything
         pass
 
     def recv(self, *args, **kwargs):
         # fix it
+        # for emitter, it doesn't matter
+        # for collector, it must receive a raw_data
+        # for worker, it must receive a number as a data and then receive a END_SIGNAL to finish the execution
         pass

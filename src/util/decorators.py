7a39265a79d6f7e95e182d1e16b45c0facba79c4
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2024-03-12 20:39:06 -0300

    fix: change decoratos log level

--- src/util/decorators.py
+++ src/util/decorators.py
@@ -14,6 +14,6 @@ def timeit(func):
         result = func(*args, **kwargs)
         end_time = time.perf_counter()
         total_time = end_time - start_time
-        logger.debug(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+        logger.info(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
         return result
     return timeit_wrapper

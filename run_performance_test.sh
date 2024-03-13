a276808773fc56637c29f2fc0561effd56eb524a
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2024-03-12 23:37:13 -0300

    fix: script

--- run_performance_test.sh
+++ run_performance_test.sh
@@ -6,24 +6,19 @@ until_number=$2
 command="mpirun -np $processes python app.py --mode parallel --until-number $until_number"
 echo "Running: $command"
 
-$command > /tmp/result.txt
 echo "Try 1"
-cat /tmp/result.txt | grep Took
-
 $command > /tmp/result.txt
+
 echo "Try 2"
-cat /tmp/result.txt | grep Took
-
 $command > /tmp/result.txt
+
 echo "Try 3"
-cat /tmp/result.txt | grep Took
-
 $command > /tmp/result.txt
+
 echo "Try 4"
-cat /tmp/result.txt | grep Took
-
 $command > /tmp/result.txt
+
 echo "Try 5"
-cat /tmp/result.txt | grep Took
+$command > /tmp/result.txt
 
 exit 0
